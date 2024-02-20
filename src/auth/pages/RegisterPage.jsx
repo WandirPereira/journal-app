c43dc29a4ac09591a7cc35f72d9ab4e36397bc00
WandirPereira <wandir@gmail.com>
2024-02-20 20:56:17 -0300

    Adicionando animações

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -62,8 +62,11 @@ export const RegisterPage = () => {
 
   return (
     <AuthLayout title="Criar conta">
-      <h1>FormValid: {isFormValid ? "Válido" : "Incorreto"}</h1>
-      <form onSubmit={onSubmit}>
+      {/* <h1>FormValid: {isFormValid ? "Válido" : "Incorreto"}</h1> */}
+      <form
+        onSubmit={onSubmit}
+        className="animate__animated animate__fadeIn animate__faster"
+      >
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
